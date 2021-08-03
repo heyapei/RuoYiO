0ddf578b7fffa1e978b1e852eb572481358da8bb
heyapei <15518901416@163.com>
2021-08-03 18:29:52 +0800

    feat(配置、代码): 将mybatis更新为mybatis-plus按照官网文档说明
    官方文档是http://doc.ruoyi.vip/ruoyi/document/cjjc.html#%E9%9B%86%E6%88%90mybatis-plus%E5%AE%9E%E7%8E%B0mybatis%E5%A2%9E%E5%BC%BA
    处理的很好 实现很完美

---
+++ ruoyi-system/src/main/java/com/ruoyi/system/controller/SysStudentController.java
@@ -0,0 +1,126 @@
+package com.ruoyi.system.controller;
+
+import java.util.Arrays;
+import java.util.List;
+import org.apache.shiro.authz.annotation.RequiresPermissions;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import com.ruoyi.common.annotation.Log;
+import com.ruoyi.common.core.controller.BaseController;
+import com.ruoyi.common.core.domain.AjaxResult;
+import com.ruoyi.common.core.page.TableDataInfo;
+import com.ruoyi.common.enums.BusinessType;
+import com.ruoyi.common.utils.poi.ExcelUtil;
+import com.ruoyi.system.domain.SysStudent;
+import com.ruoyi.system.service.ISysStudentService;
+
+/**
+ * 学生信息Controller
+ * 
+ * @author ruoyi
+ */
+@Controller
+@RequestMapping("/system/student")
+public class SysStudentController extends BaseController
+{
+    private String prefix = "system/student";
+
+    @Autowired
+    private ISysStudentService sysStudentService;
+
+    @RequiresPermissions("system:student:view")
+    @GetMapping()
+    public String student()
+    {
+        return prefix + "/student";
+    }
+
+    /**
+     * 查询学生信息列表
+     */
+    @RequiresPermissions("system:student:list")
+    @PostMapping("/list")
+    @ResponseBody
+    public TableDataInfo list(SysStudent sysStudent)
+    {
+        startPage();
+        List<SysStudent> list = sysStudentService.queryList(sysStudent);
+        return getDataTable(list);
+    }
+
+    /**
+     * 导出学生信息列表
+     */
+    @RequiresPermissions("system:student:export")
+    @Log(title = "学生信息", businessType = BusinessType.EXPORT)
+    @PostMapping("/export")
+    @ResponseBody
+    public AjaxResult export(SysStudent sysStudent)
+    {
+        List<SysStudent> list = sysStudentService.queryList(sysStudent);
+        ExcelUtil<SysStudent> util = new ExcelUtil<SysStudent>(SysStudent.class);
+        return util.exportExcel(list, "student");
+    }
+
+    /**
+     * 新增学生信息
+     */
+    @GetMapping("/add")
+    public String add()
+    {
+        return prefix + "/add";
+    }
+
+    /**
+     * 新增保存学生信息
+     */
+    @RequiresPermissions("system:student:add")
+    @Log(title = "学生信息", businessType = BusinessType.INSERT)
+    @PostMapping("/add")
+    @ResponseBody
+    public AjaxResult addSave(SysStudent sysStudent)
+    {
+        return toAjax(sysStudentService.save(sysStudent));
+    }
+
+    /**
+     * 修改学生信息
+     */
+    @GetMapping("/edit/{studentId}")
+    public String edit(@PathVariable("studentId") Long studentId, ModelMap mmap)
+    {
+        SysStudent sysStudent = sysStudentService.getById(studentId);
+        mmap.put("sysStudent", sysStudent);
+        return prefix + "/edit";
+    }
+
+    /**
+     * 修改保存学生信息
+     */
+    @RequiresPermissions("system:student:edit")
+    @Log(title = "学生信息", businessType = BusinessType.UPDATE)
+    @PostMapping("/edit")
+    @ResponseBody
+    public AjaxResult editSave(SysStudent sysStudent)
+    {
+        return toAjax(sysStudentService.updateById(sysStudent));
+    }
+
+    /**
+     * 删除学生信息
+     */
+    @RequiresPermissions("system:student:remove")
+    @Log(title = "学生信息", businessType = BusinessType.DELETE)
+    @PostMapping("/remove")
+    @ResponseBody
+    public AjaxResult remove(String ids)
+    {
+        return toAjax(sysStudentService.removeByIds(Arrays.asList(ids)));
+    }
+}
